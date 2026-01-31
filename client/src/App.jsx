127812ce855f30bb3bcc903e6803bdf4f1a68a8e
Mayank <mayankbaoniya9@gmail.com>
2026-01-31 18:38:47 +0530

    Add axios, cors, and lucide-react dependencies; implement note fetching and rate limiting in HomePage; create Navbar, NoteCard, and RateLimitedUI components; enhance App layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,8 +6,9 @@ import toast from "react-hot-toast"
 
 const App = () => {
   return (
-    <div data-theme="forest" className='h-screen'>
-      <button className='btn btn-error'>Click Me</button>
+    <div data-theme="forest" className='relative h-full w-full'>
+            <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
+
     <Routes>
       <Route path='/' element= { <HomePage/> } />
       <Route path='/create' element= { <CreatePage/> } />
--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,23 @@
 import express from 'express'
-import notesRoutes from './routes/notesRoutes.js'
-import productsRoutes from './routes/productsRoutes.js'
-import { connectDB } from './config/db.js';
+import cors from 'cors';
 import dotenv from 'dotenv'
+
+import notesRoutes from './routes/notesRoutes.js'
+import { connectDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 5001
-
 connectDB()
 
 // middleware 
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
 app.use(express.json()) // This middleware is used to parse JSON bodies: req.body
 app.use(rateLimiter)
 
@@ -23,8 +28,6 @@ app.use(rateLimiter)
 // })
 
 app.use('/api/notes', notesRoutes)
-app.use('/api/products', productsRoutes)
-
 
 connectDB().then(() => {
     app.listen(PORT, () => {
