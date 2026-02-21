ee9eecd44dcdfcf62fce8d972800f0a56ca7eb05
Mayank <mayankbaoniya9@gmail.com>
2026-02-21 19:44:16 +0530

    chore: update .gitattributes for log and editor file exclusions; add package.json and package-lock.json for project configuration

--- server/src/server.js
+++ server/src/server.js
@@ -1,36 +1,47 @@
-import express from 'express'
-import cors from 'cors';
-import dotenv from 'dotenv'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
 
-import notesRoutes from './routes/notesRoutes.js'
-import { connectDB } from './config/db.js';
-import rateLimiter from './middleware/rateLimiter.js';
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001
-connectDB()
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-// middleware 
-app.use(
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
     cors({
       origin: "http://localhost:5173",
     })
   );
-app.use(express.json()) // This middleware is used to parse JSON bodies: req.body
-app.use(rateLimiter)
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
 
-// custom middleware
+// our simple custom middleware
 // app.use((req, res, next) => {
-//    console.log(`Req method is ${req.method} and Req URL is ${req.url}`);
-//    next()
-// })
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
 
-app.use('/api/notes', notesRoutes)
+app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`)
-    })
-})
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
+});
